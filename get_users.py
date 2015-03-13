64205484c4f188fad6e48296a162995002f898c6
Jordon Wing <jordonwing@berkeley.edu>
2015-03-13 15:00:52 -0700

    Added documentation to get_users function.

--- get_users.py
+++ get_users.py
@@ -1,8 +1,15 @@
 from users.models import UserProfile
 
 def get_users():
+	"""
+		Returns a dictionary of users, in the form:
+		{<user_type>:[list of email addresses]}
+
+		where user_type is the INTEGER representation of the type of user (see models.py on the server)
+	"""
     users = UserProfile.objects.all()
     result = {t[0]:[] for t in UserProfile.USER_TYPES}
+    
     for user in users:
         result[user.user_type].append(user.user.email)
     return result
