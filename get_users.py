8b114522a5d64c8a974036574830cab84e191f42
Jordon Wing <jordonwii@gmail.com>
2015-03-16 18:32:58 -0700

    Converted tabs to spaces in get_users

--- get_users.py
+++ get_users.py
@@ -1,12 +1,12 @@
 from users.models import UserProfile
 
 def get_users():
-	"""
-		Returns a dictionary of users, in the form:
-		{<user_type>:[list of email addresses]}
+    """
+        Returns a dictionary of users, in the form:
+        {<user_type>:[list of email addresses]}
 
-		where user_type is the INTEGER representation of the type of user (see models.py on the server)
-	"""
+        where user_type is the INTEGER representation of the type of user (see models.py on the server)
+    """
     users = UserProfile.objects.all()
     result = {t[0]:[] for t in UserProfile.USER_TYPES}
     
